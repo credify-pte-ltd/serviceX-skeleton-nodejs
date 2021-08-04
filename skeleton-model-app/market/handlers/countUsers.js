2e7fb4986f86472abd11599a78eb930fb929919d
Danh Nguyen <dnncntt@gmail.com>
2021-08-04 16:55:40 +0700

    change folder name

--- skeleton-model-app/market/handlers/countUsers.js
+++ skeleton-model-app/market/handlers/countUsers.js
@@ -1,54 +1,61 @@
-const { Op } = require('sequelize');
-const extractToken = require("../utils/extractToken");
+const { Op } = require("sequelize")
+const extractToken = require("../utils/extractToken")
 
-const countUsers = async (req, res, { user, credify, evaluateOffer, scopeNames }) => {
-  console.log(JSON.stringify(req.body));
-  const token = extractToken(req);
-  const validToken = await credify.auth.introspectToken(token, "oidc_client:read_user_counts");
+const countUsers = async (
+  req,
+  res,
+  { user, credify, evaluateOffer, scopeNames }
+) => {
+  console.log(JSON.stringify(req.body))
+  const token = extractToken(req)
+  const validToken = await credify.auth.introspectToken(
+    token,
+    "oidc_client:read_user_counts"
+  )
   if (!validToken) {
-    return res.status(401).send({ message: "Unauthorized" });
+    return res.status(401).send({ message: "Unauthorized" })
   }
-  const ids = req.body.ids || [];
-  let conditions = req.body.conditions || [{}];
+  const ids = req.body.ids || []
+  let conditions = req.body.conditions || [{}]
   conditions = conditions.map((c) => {
-    if (c === null) return {};
-    else return c;
-  });
+    if (c === null) return {}
+    else return c
+  })
 
+  // Your implementation here. The code below is just a reference
   try {
     const users = await user.findAll({
       where: {
-        'credifyId': {
+        credifyId: {
           [Op.notIn]: ids, // remove users who have used an offer to this data receiver.
         },
-      }
-    });
+      },
+    })
 
-    let counts = Array(conditions.length).fill(0);
-
+    let counts = Array(conditions.length).fill(0)
     conditions.forEach((c, index) => {
       if (Object.keys(c).length === 0) {
-        counts[index] = users.length;
+        counts[index] = users.length
       } else {
         users.forEach((u) => {
-          const res = evaluateOffer(u, [c], scopeNames, []);
+          const res = evaluateOffer(u, [c], scopeNames, [])
           if (res.rank === 1) {
-            counts[index] += 1;
+            counts[index] += 1
           }
-        });
+        })
       }
-    });
+    })
 
     const response = {
       data: {
-        counts
-      }
-    };
-    res.json(response);
+        counts,
+      },
+    }
+    res.json(response)
   } catch (e) {
-    console.log(JSON.stringify(e));
-    res.status(500).send({ message: e.message });
+    console.log(JSON.stringify(e))
+    res.status(500).send({ message: e.message })
   }
-};
+}
 
-module.exports = countUsers;
+module.exports = countUsers
--- skeleton-model-app/market/handlers/filterOffer.js
+++ skeleton-model-app/market/handlers/filterOffer.js
@@ -1,37 +1,35 @@
-const extractToken = require("../utils/extractToken");
+const extractToken = require("../utils/extractToken")
 
 const filterOffer = async (req, res, { user, credify, personalizeOffers }) => {
-  console.log(JSON.stringify(req.body));
-  const token = extractToken(req);
-  console.log(token);
-  const introspectResult = await credify.auth.introspectTokenReturnResult(
-    token
-  );
-  let validToken;
+  console.log(JSON.stringify(req.body))
+  const token = extractToken(req)
+  console.log(token)
+  const introspectResult = await credify.auth.introspectTokenReturnResult(token)
+  let validToken
   if (
     introspectResult.data &&
     introspectResult.data.active &&
     introspectResult.data.scope.includes("claim_provider")
   ) {
-    validToken = true;
+    validToken = true
   }
 
   if (!validToken) {
-    return res.status(401).send({ message: "Unauthorized" });
+    return res.status(401).send({ message: "Unauthorized" })
   }
-  const credifyId = req.body.credify_id;
-  const localId = req.body.local_id;
-  const offers = req.body.offers;
+  const credifyId = req.body.credify_id
+  const localId = req.body.local_id
+  const offers = req.body.offers
   if (!credifyId && !localId) {
-    return res.status(400).send({ message: "No ID found" });
+    return res.status(400).send({ message: "No ID found" })
   }
   if (offers === undefined) {
     const response = {
       data: {
         offers: [],
       },
-    };
-    return res.status(200).json(response);
+    }
+    return res.status(200).json(response)
   }
 
   try {
@@ -40,32 +38,33 @@ const filterOffer = async (req, res, { user, credify, personalizeOffers }) => {
         data: {
           offers: [],
         },
-      };
-      return res.status(200).json(response);
-    }
-    let u;
-
-    if (credifyId) {
-      const users = await user.findAll({ where: { credifyId } });
-      if (users.length !== 1) {
-        throw new Error("Not found user properly");
       }
-      u = users[0];
-    } else if (localId) {
-      u = await user.findByPk(localId);
+      return res.status(200).json(response)
     }
-    const personalizedOffers = personalizeOffers(u, offers);
+    let u
+
+    // Interaction here
+    if (credifyId) {
+      const users = await user.findAll({ where: { credifyId } })
+      if (users.length !== 1) {
+        throw new Error("Not found user properly")
+      }
+      u = users[0]
+    } else if (localId) {
+      u = await user.findByPk(localId)
+    }
+    const personalizedOffers = personalizeOffers(u, offers)
 
     const response = {
       data: {
         offers: personalizedOffers,
       },
-    };
-    res.json(response);
+    }
+    res.json(response)
   } catch (e) {
-    console.log(JSON.stringify(e));
-    res.status(500).send({ message: e.message });
+    console.log(JSON.stringify(e))
+    res.status(500).send({ message: e.message })
   }
-};
+}
 
-module.exports = filterOffer;
+module.exports = filterOffer
