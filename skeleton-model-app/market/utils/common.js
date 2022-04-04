8bc1dc1df8570479dd437135705907385c714c86
Danh Nguyen <dnncntt@gmail.com>
2022-04-04 11:44:52 +0700

    update nodesdk and scope.js

--- skeleton-model-app/market/utils/common.js
+++ skeleton-model-app/market/utils/common.js
@@ -8,7 +8,7 @@ const injectBasicProfileScope = (
     claims[`phone`] = {
       [`phone_number`]: `${user.phoneCountryCode}${user.phoneNumber}`,
       [`phone_commitment`]: commitments ? commitments[`phone`] : undefined,
-    }
+    };
   }
 
   if (shareableProfile === null || shareableProfile.includes("NAME")) {
@@ -16,14 +16,28 @@ const injectBasicProfileScope = (
       [`family_name`]: `${user.lastName}`,
       [`given_name`]: `${user.firstName}`,
       [`profile_commitment`]: commitments ? commitments[`profile`] : undefined,
-    }
+    };
+  }
+
+  if (shareableProfile.includes("DOB")) {
+    claims[`profile`] = {
+      ...claims[`profile`],
+      [`birthdate`]: `${user.dob}`,
+    };
+  }
+
+  if (shareableProfile.includes("GENDER")) {
+    claims[`profile`] = {
+      ...claims[`profile`],
+      [`gender`]: `${user.gender}`,
+    };
   }
 
   if (shareableProfile === null || shareableProfile.includes("EMAIL")) {
     claims[`email`] = {
       [`email`]: `${user.email}`,
       [`email_commitment`]: commitments ? commitments[`email`] : undefined,
-    }
+    };
   }
 
   if (shareableProfile === null || shareableProfile.includes("ADDRESS")) {
@@ -32,10 +46,10 @@ const injectBasicProfileScope = (
         [`formatted`]: `${user.address}`,
       },
       [`address_commitment`]: commitments ? commitments[`address`] : undefined,
-    }
+    };
   }
-}
+};
 
 module.exports = {
   injectBasicProfileScope,
-}
+};
