5ca6f312a597881e086b3280024713232bd1f3e8
ngo275 <ngo38114@gmail.com>
2022-05-30 08:09:54 +0700

    Separated app and server

--- skeleton-model-app/market/app.js
+++ skeleton-model-app/market/app.js
@@ -16,14 +16,10 @@ app.use(morgan("combined"))
 app.use(bodyParser.json({ limit: "50mb" }))
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }))
 
-const port = process.env.PORT || 8000
 app.get("/", (req, res) => {
   res.send("health check")
 })
 app.get("/favicon.ico", (req, res) => res.status(204))
 app.use("/v1", v1({ db }))
 
-/// Start server
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`)
-})
+module.exports = app;
