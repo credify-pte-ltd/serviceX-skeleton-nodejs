a959f90f00fa6cfb36dbc503a646a97bc0ffa4ad
credifythanhtran <tranthanh.it.95@gmail.com>
2022-09-14 09:28:24 +0700

    fix indents

--- skeleton-model-app/market/handlers/simulation.js
+++ skeleton-model-app/market/handlers/simulation.js
@@ -1,18 +1,18 @@
 
 
 const simulation = async (req, res, { db, credify }) => {
-    try {
+  try {
 
-        const productType = req.body.productType
-        const providerIds = req.body.providerIds
-        const inputs = req.body.inputs
+    const productType = req.body.productType
+    const providerIds = req.body.providerIds
+    const inputs = req.body.inputs
 
-        const response = await credify.offer.simulate(productType, providerIds, inputs)
+    const response = await credify.offer.simulate(productType, providerIds, inputs)
 
-        res.status(200).json(response)
-    } catch (e) {
-        res.status(500).send({ message: e.message })
-    }
+    res.status(200).json(response)
+  } catch (e) {
+    res.status(500).send({ message: e.message })
+  }
 }
 
 module.exports = simulation;
